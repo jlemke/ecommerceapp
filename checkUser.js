08e019a06a44a31cfb3cc8686d98304faaeb6b99
jlemke <jlemke3@madisoncollege.edu>
2022-05-12 19:15:49 -0500

    Created checkUser from textbook
    Reformatted, browsed code

---
+++ checkUser.js
@@ -0,0 +1,30 @@
+import { Auth } from 'aws-amplify';
+
+const checkUser = async (updateUser) => {
+
+  const userData = await Auth
+    .currentSession()
+    .catch(err => console.log('error: ', err));
+
+  if (!userData) {
+    console.log('userData: ', userData);
+    updateUser({});
+    return;
+  }
+
+  // Destructure userData, get idToken.payload
+  const { idToken: { payload }} = userData;
+
+  // Check if user is admin, set isAuthorized
+  const isAuthorized =
+    payload['cognito:groups'] &&
+    payload['cognito:groups'].includes('Admin');
+
+  
+  updateUser({
+    username: payload['cognito:username'],
+    isAuthorized
+  });
+};
+
+export default checkUser;
