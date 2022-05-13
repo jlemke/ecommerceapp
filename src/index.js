1b500548d318691a30034da10f16846d6c0853d3
jlemke <jlemke3@madisoncollege.edu>
2022-05-12 19:09:08 -0500

    Added to index, created Container.js
    Added amplify configuring to index.js, changed to use Router component.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import Router from './Router'
+import 'antd/dist/antd.css' //AntD styling
+
+import { Amplify } from 'aws-amplify'
+import config from './aws-exports'
+
+
+Amplify.configure(config)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router />
   </React.StrictMode>
 );
 
